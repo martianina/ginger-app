aea1977c7b62a980955f911c2b292ca0d06b5cec
Ryan Kilbride <rp.kilbride@Ryans-MacBook-Air.local>
2025-08-07 20:12:51 -0400

    m

--- src/app/AppWrappers.tsx
+++ src/app/AppWrappers.tsx
@@ -8,18 +8,10 @@ import '@/styles/Contact.css';
 import '@/styles/MiniCalendar.css';
 import '@/styles/index.css';
 
-import dynamic from 'next/dynamic';
-
-const _NoSSR = ({ children }) => <React.Fragment>{children}</React.Fragment>;
-
-const NoSSR = dynamic(() => Promise.resolve(_NoSSR), {
-  ssr: false,
-});
-
 export default function AppWrappers({ children }: { children: ReactNode }) {
   return (
     <SessionProvider>
-      <NoSSR>{children}</NoSSR>
+      {children}
     </SessionProvider>
   );
 }
